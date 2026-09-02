05023a0914690d101359f663a4f31277fe362fdf
Pooja Gaygaye <poojagaygaye3@gmail.com>
2026-09-02 21:36:35 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,60 @@
+package com.pooja.employee_management_system.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String category;
+
+    private String email;
+
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String category, String email, String password) {
+        this.id = id;
+        this.category = category;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
